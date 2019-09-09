e5069df36f8e846ea1460c8603872ccba2722f28
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-09-09 13:09:37 -0500

    typo

--- scrape_mars.py
+++ scrape_mars.py
@@ -121,7 +121,7 @@ def scrape():
     items = soup.find_all('div', class_='item')
     hemisphere_image_urls = []
 
-    print("how many items:" + str(len(items))
+    print("how many items:" + str(len(items)))
     for item in items:
         print("Hemesphere iteration")
         item_url = item.find('h3').text
